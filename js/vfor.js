cb58a9ab8f21ce6e79e9d3560172f225b65917fc
zigzag <447587073@qq.com>
2016-10-26 19:22:23 +0800

    header.html

---
+++ js/vfor.js
@@ -0,0 +1,23 @@
+
+/*
+* 测试用
+* components 通信 实例
+*/
+
+
+(function(){
+
+    new Vue({
+        el: 'body',
+        data:{
+            items: [11,22,33]
+        },
+        components: {}
+    });
+
+})(window);
+
+
+
+
+
