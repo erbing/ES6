cd87f74e984a613314639701f40a4ee82b1d1c01
zigzag <447587073@qq.com>
2017-02-07 15:38:41 +0800

    success lazyMan

--- js/lazyman.js
+++ js/lazyman.js
@@ -1,16 +1,50 @@
 // 构造函数
 function _lazyMan(name) {
     this.tasks = [];
-    console.log(name);
+    var that = this;
+    var fn = (function(n) {
+        var name = n;
+        return function() {
+            console.log(name);
+            that.next();
+        }
+    })(name)
+    this.tasks.push(fn);
+    setTimeout(function() {
+        that.next();
+    },0)
+}
+
+// 事件调度函数
+_lazyMan.prototype.next = function() {
+    var fn = this.tasks.shift();
+    fn && fn();
 }
 
 // 构造函数原型上的  sleep fn
 _lazyMan.prototype.sleep = function() {
-    console.log('i am sleepping');
+    var that = this;
+    var fn = (function() {
+        return function() {
+            console.log('i am sleepping');
+            that.next();
+        }
+    })()
+    this.tasks.push(fn);
+    return this;    // 实现链式调用
 }
 
+// 构造函数原型上的  eat fn
 _lazyMan.prototype.eat = function() {
-    console.log('i am eatting');
+    var that = this;
+    var fn = (function() {
+        return function() {
+            console.log('i am eatting');
+            that.next();
+        }
+    })()
+    this.tasks.push(fn);
+    return this;    // 实现链式调用
 }
 
 /* 封装 */
@@ -18,4 +52,5 @@ function lazyMan(name) {
     return new _lazyMan(name);
 }
 
-lazyMan('zz').eat();
+lazyMan('zz').eat().sleep();
+lazyMan('zz').sleep().eat();
